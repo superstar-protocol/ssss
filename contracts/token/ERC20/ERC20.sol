742acbfd12efe9b888239b0ddb6744dd5b6069b2
superstar-protocol <82321763+superstar-protocol@users.noreply.github.com>
2021-04-11 17:31:55 +0300

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -35,6 +35,12 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
 
     mapping (address => mapping (address => uint256)) private _allowances;
 
+uint256 public constant decimals = 9;
+
+uint256 private constant DECIMALS = 9;
+uint256 private constant MAX_UINT256 = ~uint256(0);
+uint256 private constant INITIAL_FRAGMENTS_SUPPLY = 100 * 9 * 9;
+
     uint256 private _totalSupply;
 
     string private _name;
